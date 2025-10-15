836aa133f729ce299e554d77da4e4e2e51e452b7
Shaurya926 <santoshdubey7089@gmail.com>
2025-10-15 16:00:08 +0530

    DMA_m

--- DMA/DMA_water_problem.cpp
+++ DMA/DMA_water_problem.cpp
@@ -1,12 +1,13 @@
 #include <stdio.h>
-
 #include <iostream>
+ 
 using namespace std;
+
 int main() 
 {
     int height[]={1,8,6,2,5,4,8,3,7};
     int n=sizeof(height)/sizeof(height[0]);
-int i = 0;
+        int i = 0;
         int j = n- 1;
         int res = 0;
 
