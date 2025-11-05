6a1657986751b0759cd732990112b156d600436c
Shaurya926 <santoshdubey7089@gmail.com>
2025-11-05 10:08:03 +0530

    IF_and_else_statement

---
+++ IF_else_statement/Q1.c
@@ -0,0 +1,17 @@
+//Write a C program to check whether a number is positive, negative or zero.
+#include <stdio.h>  
+int main() {  
+    int number;  
+    printf("Enter a number: ");  
+    scanf("%d", &number);  
+    
+    if (number > 0) {  
+        printf("The number is positive.\n");  
+    } else if (number < 0) {  
+        printf("The number is negative.\n");  
+    } else {  
+        printf("The number is zero.\n");  
+    }  
+    
+    return 0;  
+}
