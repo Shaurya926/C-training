a5c3b8bb1ae8d80d89a779ca64d11bc0d9739af0
Shaurya926 <santoshdubey7089@gmail.com>
2025-11-08 10:10:03 +0530

    IF_and_else_statement

---
+++ IF_else_statement/to_make_condition.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+// WAP to make a simple calculator using if..else statement
+// + , - , * , /
+// Input : 10 5 +
+// Output : The Addition of Two Numbers : 15
+int main()
+{
+    int a,b;
+    char ch;
+    printf("Enter First Numbers : ");
+    scanf("%d",&a);
+    printf("Enter Second Numbers : ");
+    scanf("%d",&b);
+
+    printf("Enter Your Choice : ");
+    scanf(" %c",&ch);
+    if(ch=='+')
+    {
+        printf("\nThe Addition of Two Numbers : %d",a+b);
+    }
+    else if(ch=='-')
+    {
+        printf("\nThe Substraction of Two Numbers : %d",a-b);
+    }
+    else if(ch=='*')
+    {
+        printf("\nThe multiplication of Two Numbers : %d",a*b);
+    }
+    else if(ch=='/')
+    {
+        printf("\nThe Division of Two Numbers : %.2f",(float)a/b);
+    }
+    else
+    {
+        printf("\nInvalid Input");
+    }
+}
