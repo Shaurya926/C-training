eefdf4316fea3ea93d3115ce7df08ed9a484b856
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-02 23:32:08 +0530

    Print_Different_Pattern

---
+++ Pattern/Triangle_no_4_pattern.c
@@ -0,0 +1,21 @@
+// Print opposite triangle pattern
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf(" ");
+        }
+        for (int k = n; k > i; k--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
