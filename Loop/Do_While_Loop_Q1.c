aae09af0fbcb6a25c57b34242da5a9ccf6ea6e33
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-08 00:10:17 +0530

    Different_Loop_Method

--- Loop/Do_While_Loop_Q1.c
+++ Loop/Do_While_Loop_Q1.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 // Program to demonstrate a do-while loop in C
+// Diffrence between while loop and do-while loop is that do-while loop executes the block of code at least 
+// once before checking the condition.
 int main()
 {
     int i =1;
---
+++ Loop/For_Loop_Q1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// Program to demonstrate a for loop in C
+int main() {
+    int i;
+    // Loop from 1 to 10
+    for(i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
