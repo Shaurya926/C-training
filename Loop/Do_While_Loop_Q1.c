7b206752c18f197b45ce31878cac5a066423b194
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-08 00:08:47 +0530

    Different_Loop_Method

---
+++ Loop/Do_While_Loop_Q1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// Program to demonstrate a do-while loop in C
+int main()
+{
+    int i =1;
+    do{
+    printf("%d\n", i);
+    i++;
+    }
+    while(i<=10);
+    return 0;
+}
