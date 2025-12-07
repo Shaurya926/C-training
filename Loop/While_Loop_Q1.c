e7f11245996bae4368bfd8e70ac0eca26ca4659b
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-08 00:06:12 +0530

    Different_Loop_Method

---
+++ Loop/While_Loop_Q1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// Program to demonstrate a while loop in C
+int main(){
+    int i;
+    // Loop from 1 to 10
+    i = 1;
+    while(i <= 10){
+        printf("%d\n", i);
+        i++;
+    }
+    return 0;
+}
