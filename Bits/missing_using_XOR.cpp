0be314f044dd5b544c7063e8249daf074634eb29
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-10 00:02:38 +0530

    Bits_Question

--- Bits/missing_using_XOR.cpp
+++ Bits/missing_using_XOR.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-   int arr[4]={1,2,3,5};
+   int arr[7]={1,6,7,11,2,3,5};
    int n=5;
    int Xor=0;
     for(int i=0;i<4;i++)
@@ -15,3 +15,6 @@ int main()
     printf("The missing number is %d",Xor);
     return 0;
 }
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+// This code finds the missing number in an array of size n-1 containing numbers from 1 to n using XOR operation.
