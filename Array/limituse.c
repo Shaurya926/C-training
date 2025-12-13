900b89a522d7eb775d299471b66d713dfce1e3a3
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-13 23:55:26 +0530

    Array

--- Array/limituse.c
+++ Array/limituse.c
@@ -1,1 +1,20 @@
 #include<limits.h>
+#include<stdio.h>
+void main()
+{
+    printf("The value of CHAR_BIT is : %d\n", CHAR_BIT);
+    printf("The value of CHAR_MAX is : %d\n", CHAR_MAX);
+    printf("The value of CHAR_MIN is : %d\n", CHAR_MIN);
+    printf("The value of INT_MAX is : %d\n", INT_MAX);
+    printf("The value of INT_MIN is : %d\n", INT_MIN);
+    printf("The value of LONG_MAX is : %ld\n", LONG_MAX);
+    printf("The value of LONG_MIN is : %ld\n", LONG_MIN);
+    printf("The value of SCHAR_MAX is : %d\n", SCHAR_MAX);
+    printf("The value of SCHAR_MIN is : %d\n", SCHAR_MIN);
+    printf("The value of SHRT_MAX is : %d\n", SHRT_MAX);
+    printf("The value of SHRT_MIN is : %d\n", SHRT_MIN);
+    printf("The value of UCHAR_MAX is : %d\n", UCHAR_MAX);
+    printf("The value of UINT_MAX is : %u\n", UINT_MAX);
+    printf("The value of ULONG_MAX is : %lu\n", ULONG_MAX);
+    printf("The value of USHRT_MAX is : %d\n", USHRT_MAX);
+}
