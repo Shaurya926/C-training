e5bef8e706b6d760b1304987f04b9a6036c3f513
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-13 23:57:51 +0530

    Array

--- Array/merge_2_array.cpp
+++ Array/merge_2_array.cpp
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main(){
-    int a[3]={1,2,3};
-    int b[3]={4,5,6};
-    int c[6];
-    for(int i=0;i<3;i++)
+    int a[5]={1,2,3,4,5};
+    int b[5]={4,5,6,8,9};
+    int c[10];
+    for(int i=0;i<5;i++)
     {
         c[i]=a[i];
-        c[i+3]=b[i];
-    }printf("c[6]=");
-    for(int i=0;i<6;i++)
+        c[i+5]=b[i];
+    }printf("c[10]=");
+    for(int i=0;i<10;i++)
     {
        printf("%d ",c[i]);
 
