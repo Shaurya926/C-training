1bbb254fe3b75196d496d2d375762bf13ffb9081
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-13 23:53:19 +0530

    2D_Array

---
+++ 2D_Array/Create_2D_Array.c
@@ -0,0 +1,31 @@
+// Creating 2D array.
+#include <stdio.h>
+int main() {
+    int rows, cols;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int arr[rows][cols];
+
+    // Input elements into the 2D array
+    printf("Enter elements of the array:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    // Display the 2D array
+    printf("The 2D array is:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
