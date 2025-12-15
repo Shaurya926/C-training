6b5a6366cbe1da895f141039c4187777ace7d8d4
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-15 23:55:55 +0530

    Array

--- Array/sim_o_3.cpp
+++ Array/sim_o_3.cpp
@@ -1,23 +1,23 @@
-#include<stdio.h>
-int  main()
+#include <stdio.h>
+int main()
 {
-    int a[5]={-4,-1,2,-7,3};
+    int a[5] = {-4, -1, 2, -7, 3};
     int sum_a[3];
-    for(int i=0;i<3;i++){
-                sum_a[i]=a[i]+a[i+1]+a[i+2];
-               
+    for (int i = 0; i < 3; i++)
+    {
+        sum_a[i] = a[i] + a[i + 1] + a[i + 2];
     }
 
-    for(int i=0 ; i<3;i++){
-        printf("%d\n",sum_a[i]);
-        
-
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%d\n", sum_a[i]);
     }
-    int great=sum_a[0];
-     for(int i=1 ; i<3;i++){
-        if(great <sum_a[i])
-        great=sum_a[i];
-     }
-     printf("%d",great);
+    int great = sum_a[0];
+    for (int i = 1; i < 3; i++)
+    {
+        if (great < sum_a[i])
+            great = sum_a[i];
+    }
+    printf("%d", great);
     return 0;
 }
