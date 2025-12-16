185d2101a9b4aefb120c7e400c01f059f80adca2
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-17 00:37:51 +0530

    Swict_Casse_Questions

---
+++ Switch_Case_Statements/How_to_use_statements.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+// Example program demonstrating switch-case statements
+int main(){
+    int a = 1;
+    switch(a){
+        case 1: printf("One\n");
+        case 2: printf("Two\n");
+        case 3: printf("Three\n");
+      
+    }
+}
