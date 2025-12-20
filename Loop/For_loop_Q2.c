36ff0c4a0ebc6e8ec7fa873e05e465f36d45e1b1
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-20 23:48:32 +0530

    Code_in_different_loop_method

---
+++ Loop/For_loop_Q2.c
@@ -0,0 +1,10 @@
+// Write a C program to print the first 10 natural numbers using a for loop.
+#include <stdio.h>
+int main() {
+    int i;
+    printf("The first 10 natural numbers are:\n");
+    for(i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
