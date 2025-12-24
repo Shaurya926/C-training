aec0789210e246540ab0a481eba74634f162a86d
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-24 23:53:30 +0530

    Array

--- Array/arraypalindrome.cpp
+++ Array/arraypalindrome.cpp
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
-    int arr[5]={1,2,3,2,1};
+    int arr[6]={1,2,3,2,1,6};
     int check=1;
-    for(int i=0;i<5/2;i++){
-        if (arr[i]!=arr[5-1-i]){
+    for(int i=0;i<6/2;i++){
+        if (arr[i]!=arr[6-1-i]){
             check=0;
             break;
         }
--- Array/findingsum.cpp
+++ Array/findingsum.cpp
@@ -1,11 +1,11 @@
 //Finding sum of two elements in an array
 #include<stdio.h>
 int main(){
-    int arr[8]={1,2,3,4,5,6,7,8};
-    int k=5;
+    int arr[10]={1,2,3,4,5,6,7,8,9,10};
+    int k=7;
     {
-        for(int i=0;i<8;i++){
-            for(int j=0;j<8;j++){
+        for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++){
                 if(arr [i] + arr[j]==k)
                 {
                 printf("%d %d\n",arr[i],arr[j],i,j);
