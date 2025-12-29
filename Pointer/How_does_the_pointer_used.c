e7d054dec85294c80830793095afd4288625b1fd
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-29 14:37:42 +0530

    Pointer

---
+++ Pointer/How_does_the_pointer_used.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void updateValue(int *ptr) {
+    *ptr = 20; // Update the value at the address pointed to by ptr
+}
+int main() {
+    int num = 10;
+    printf("Before update: %d\n", num);
+    
+    updateValue(&num); // Pass the address of num to the function
+    
+    printf("After update: %d\n", num);
+    return 0;
+}
