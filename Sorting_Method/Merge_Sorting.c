a3d40333ea65472c5ad21ff80c25cabfde1c1d49
Shaurya926 <santoshdubey7089@gmail.com>
2026-03-24 10:16:46 +0530

    Sorting Method

--- Sorting_Method/Merge_Sorting.c
+++ Sorting_Method/Merge_Sorting.c
@@ -2,7 +2,8 @@
 // Merge Sort implementation in C
 // Divide the array into halves, sort each half, and then merge them back together.
 
-void merge(int arr[], int left, int mid, int right) {
+void merge(int arr[], int left, int mid, int right)
+{
     int i, j, k;
     int n1 = mid - left + 1;
     int n2 = right - mid;
@@ -17,14 +18,18 @@ void merge(int arr[], int left, int mid, int right) {
         R[j] = arr[mid + 1 + j];
 
     // Merge the temporary arrays back into arr[left..right]
-    i = 0; // Initial index of first sub-array
-    j = 0; // Initial index of second sub-array
+    i = 0;    // Initial index of first sub-array
+    j = 0;    // Initial index of second sub-array
     k = left; // Initial index of merged sub-array
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
             arr[k] = L[i];
             i++;
-        } else {
+        }
+        else
+        {
             arr[k] = R[j];
             j++;
         }
@@ -32,26 +37,31 @@ void merge(int arr[], int left, int mid, int right) {
     }
 
     // Copy the remaining elements of L[], if there are any
-    while (i < n1) {
+    while (i < n1)
+    {
         arr[k] = L[i];
         i++;
         k++;
     }
 
     // Copy the remaining elements of R[], if there are any
-    while (j < n2) {
+    while (j < n2)
+    {
         arr[k] = R[j];
         j++;
         k++;
     }
 }
-int main(){
+int main()
+{
     int arr[] = {38, 27, 43, 3, 9, 82, 10};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    
+    int n = sizeof(arr) / sizeof(arr[0]);
+
     // Perform merge sort
-    void mergeSort(int arr[], int left, int right) {
-        if (left < right) {
+    void mergeSort(int arr[], int left, int right)
+    {
+        if (left < right)
+        {
             int mid = left + (right - left) / 2;
 
             // Sort first and second halves
